d8d714c7902b1c1a4f138f69c57e2ef09192aa5c
SRMLabHainu <100122453+SRMLabHainu@users.noreply.github.com>
2022-03-22 16:59:44 +0800

    Update Colocalization.cpp
    CBC algorithm

--- DecodeSTORM_CPPDLL/DecodeSTORM_CPPDLL/Colocalization.cpp
+++ DecodeSTORM_CPPDLL/DecodeSTORM_CPPDLL/Colocalization.cpp
@@ -1,12 +1,12 @@
 #include "stdafx.h"
 #include "Colocalization.h"
 
-float Radius, Rmax, Step, ColoThres;
+float  Rmax, Step, ColoThres;
 double width, height;
-double* Doc1Array, * Doc2Array;
-int Doc1Size, Doc2Size;
-float Doc1Range, Doc2Range;
-rowvec DoC_Results;
+double* CBC1Array, * CBC2Array;
+int CBC1Size, CBC2Size;
+float CBC1Range, CBC2Range;
+rowvec CBC_Results;
 
 vec Colocalization::NumOfRangeSearch(const mat& matrix_1, const mat& matrix_2, double radius) {
 	vec nParts;
@@ -24,28 +24,7 @@ vec Colocalization::NumOfRangeSearch(const mat& matrix_1, const mat& matrix_2, d
 }
 
 
-struct Res Colocalization::Lr_Subfun(const mat& matrix_1, const mat& matrix_2) {
-	int k;
-	if (matrix_1.n_rows != matrix_2.n_rows) {
-		k = 0;
-	}
-	else if (!approx_equal(matrix_1, matrix_2, "absdiff", 0)) {
-		k = 0;
-	}
-	else if (approx_equal(matrix_1, matrix_2, "absdiff", 0)) {
-		k = 1;
-	}
 
-	vec nParts = NumOfRangeSearch(matrix_1, matrix_2, Radius);
-	vec Kfuncans = nParts - k;
-	//vec Density = nParts / (PI * pow(Radius, 2));
-	vec Lr = pow(pow(SizeROI, 2) * Kfuncans / (matrix_2.n_rows - 1) / PI, 0.5);
-	Res res;
-	res.Lr = Lr;
-	//res.Density = Density;
-	return res;
-
-}
 
 
 vector<double> Colocalization::R_nearestSearch(vector<double>& x1, vector<double>& y1, vector<double>& x2, vector<double>& y2) {
@@ -143,50 +122,29 @@ void Colocalization::ColocalizationCal(const mat& inLoc1, const mat& inLoc2, con
 {
 	
 
-	mat zero1 = zeros(inLoc1.n_rows, 2);
-	mat zero2 = zeros(inLoc2.n_rows, 2);
-	mat inLoc1Ext = join_rows(inLoc1, zero1);
-	mat inLoc2Ext = join_rows(inLoc2, zero2);
-	mat inLocExt = join_cols(inLoc1Ext, inLoc2Ext);
 
 	SizeROI = width * height;
-	//Caculate Lr and Density from localization of all channel
-	Res res = Lr_Subfun(inLocExt, inLocExt);
-	inLoc1Ext.col(12) = res.Lr.rows(0, size(inLoc1Ext, 0) - 1);
-	inLoc2Ext.col(12) = res.Lr.rows(size(inLoc1Ext, 0), size(res.Lr, 0) - 1);
+
+	vec x1 = inLoc1.col(1);
+	vec y1 = inLoc1.col(2);
+	vec x2 = inLoc2.col(1);
+	vec y2 = inLoc2.col(2);
 
 
-
-	double Lr_Threshold = (size(inLocExt, 0) / SizeROI) * PI * pow(Radius, 2);
-	//Lr_r above threshold
-	inLoc1Ext.col(13) = conv_to< colvec >::from(inLoc1Ext.col(12) > Lr_Threshold);
-	inLoc2Ext.col(13) = conv_to< colvec >::from(inLoc2Ext.col(12) > Lr_Threshold);
-
-	uvec index_thre_channel_1 = find(inLoc1Ext.col(13) == 1);
-	uvec index_thre_channel_2 = find(inLoc2Ext.col(13) == 1);
-
-	mat thre_channel_1 = inLoc1Ext.rows(index_thre_channel_1);
-	mat thre_channel_2 = inLoc2Ext.rows(index_thre_channel_2);
-	vec x1 = thre_channel_1.col(1);
-	vec y1 = thre_channel_1.col(2);
-	vec x2 = thre_channel_2.col(1);
-	vec y2 = thre_channel_2.col(2);
-
-
-	double D1max = sum(inLoc1Ext.col(13) == 1) / pow(SizeROI, 2);
-	double D2maxCh1Ch2 = sum(inLoc2Ext.col(13) == 1) / pow(SizeROI, 2);
-	vec D2maxCh2Ch1 = NumOfRangeSearch(thre_channel_1, thre_channel_2, Rmax) / pow(Rmax, 2);
-	mat N11 = zeros(thre_channel_1.n_rows, ceil(Rmax / Step));
-	mat N12 = zeros(thre_channel_1.n_rows, ceil(Rmax / Step));
-	mat N22 = zeros(thre_channel_2.n_rows, ceil(Rmax / Step));
-	mat N21 = zeros(thre_channel_2.n_rows, ceil(Rmax / Step));
+	double D1max = inLoc1.n_rows / pow(SizeROI, 2);
+	double D2maxCh1Ch2 = inLoc2.n_rows / pow(SizeROI, 2);
+	vec D2maxCh2Ch1 = NumOfRangeSearch(inLoc1, inLoc2, Rmax) / pow(Rmax, 2);
+	mat N11 = zeros(inLoc1.n_rows, ceil(Rmax / Step));
+	mat N12 = zeros(inLoc1.n_rows, ceil(Rmax / Step));
+	mat N22 = zeros(inLoc2.n_rows, ceil(Rmax / Step));
+	mat N21 = zeros(inLoc2.n_rows, ceil(Rmax / Step));
 
 
 	vector<double> x1_ = conv_to< vector<double> >::from(x1);
 	vector<double> y1_ = conv_to< vector<double> >::from(y1);
 	vector<double> x2_ = conv_to< vector<double> >::from(x2);
 	vector<double> y2_ = conv_to< vector<double> >::from(y2);
-	//Calculate Ch1->Ch2 DoC and Ch2->Ch1 Doc
+	//Calculate Ch1->Ch2 CBC and Ch2->Ch1 CBC
 	mat num_points;
 	for (int i = 1; i <= ceil(Rmax / Step); i++)
 	{
@@ -239,32 +197,30 @@ void Colocalization::ColocalizationCal(const mat& inLoc1, const mat& inLoc2, con
 	vec zero_SA2_ = zeros(index_SA2_id.n_rows, 1);
 	SA2.rows(index_SA2_id) = zero_SA2_;
 
-	vec channel_1_Doc = zeros(inLoc1Ext.n_rows, 1);
-	vec channel_2_Doc = zeros(inLoc2Ext.n_rows, 1);
-	//Caculate Doc
-	vec NND1 = NearestSearch(thre_channel_2, thre_channel_1);
-	channel_1_Doc.rows(index_thre_channel_1) = SA1 % exp(-NND1 / Rmax);
-	vec NND2 = NearestSearch(thre_channel_1, thre_channel_2);
-	channel_2_Doc.rows(index_thre_channel_2) = SA2 % exp(-NND2 / Rmax);
+	vec channel_1_CBC = zeros(inLoc1.n_rows, 1);
+	vec channel_2_CBC = zeros(inLoc2.n_rows, 1);
+	//Caculate CBC
+	vec NND1 = NearestSearch(inLoc2, inLoc1);
+	channel_1_CBC = SA1 % exp(-NND1 / Rmax);
+	vec NND2 = NearestSearch(inLoc1, inLoc2);
+	channel_2_CBC = SA2 % exp(-NND2 / Rmax);
 
-	vec Doc_1 = channel_1_Doc.rows(index_thre_channel_1);
-	vec Doc_2 = channel_2_Doc.rows(index_thre_channel_2);
-	Doc1Range = max(Doc_1) - min(Doc_1);
-	Doc2Range = max(Doc_2) - min(Doc_2);
-	Doc1Size = Doc_1.size();
-	Doc2Size = Doc_2.size();
-	Doc1Array = ArmaVecToArrayD(Doc_1);
-	Doc2Array = ArmaVecToArrayD(Doc_2);
+	CBC1Range = max(channel_1_CBC) - min(channel_1_CBC);
+	CBC2Range = max(channel_2_CBC) - min(channel_2_CBC);
+	CBC1Size = channel_1_CBC.size();
+	CBC2Size = channel_2_CBC.size();
+	CBC1Array = ArmaVecToArrayD(channel_1_CBC);
+	CBC2Array = ArmaVecToArrayD(channel_2_CBC);
 	
 	//Save percentage of colocalised Ch1 molecules and Percentage of colocalised Ch2 molecules
-	float Ch1_ColocAbove = sum(Doc_1 >= ColoThres);
-	float Ch1_Num = thre_channel_1.n_rows;
-	float Ch2_ColocAbove = sum(Doc_2 >= ColoThres);
-	float Ch2_Num = thre_channel_2.n_rows;
+	float Ch1_ColocAbove = sum(channel_1_CBC >= ColoThres);
+	float Ch1_Num = inLoc1.n_rows;
+	float Ch2_ColocAbove = sum(channel_2_CBC >= ColoThres);
+	float Ch2_Num = inLoc2.n_rows;
 	double Percent_Ch1_ColocAbove = Ch1_ColocAbove / Ch1_Num;
 	double Percent_Ch2_ColocAbove = Ch2_ColocAbove / Ch2_Num;
-	DoC_Results.zeros(2);
-	DoC_Results(0) = Percent_Ch1_ColocAbove;
-	DoC_Results(1) = Percent_Ch2_ColocAbove;
+	CBC_Results.zeros(2);
+	CBC_Results(0) = Percent_Ch1_ColocAbove;
+	CBC_Results(1) = Percent_Ch2_ColocAbove;
 
 }
